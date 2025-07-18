73c353859d3bced9d10d5ca8e096b9df3e28d4f2
A.SAI GURU SURYA TEJA <154744703+saisurya123658@users.noreply.github.com>
2025-07-18 20:21:11 +0530

    UserContext.jsx

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,11 +1,8 @@
 import { LogOut } from "lucide-react";
 import { createContext, useState } from "react";
-
 export const UserContext = createContext();
-
 export function UserProvider({ children }) {
   const [userName, setUserName] = useState("");
-
   return (
     <UserContext.Provider value={{ userName, setUserName ,LogOut}}>
       {children}
